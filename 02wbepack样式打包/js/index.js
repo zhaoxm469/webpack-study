c0d28020ae2464322ac5c741e8f99ee16fce6c84
zhaoxingming <zhaoxingming@nucarf.com>
2020-12-11 17:58:26 +0800

    调试

--- 02wbepack样式打包/js/index.js
+++ 02wbepack样式打包/js/index.js
@@ -1,3 +1,15 @@
-// eslint-disable-next-line no-undef
-$('body').append('123');
 import('../scss/reset.scss');
+// import { name } from './api';
+
+function getList() {
+    return [1, 2, 3];
+}
+
+// console.log(name);
+
+(async () => {
+    const data = await getList();
+    document.body.innerHTML = data;
+})();
+
+// $('body').append('123');
