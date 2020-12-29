2a73574145ddff702ca43525c02905d1cab1c8e3
zhaoxingming <zhaoxingming@nucarf.com>
2020-12-29 23:02:25 +0800

    目录名称更改

--- 12JS兼容性处理/webpack.config.js
+++ 12JS兼容性处理/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/main.js',
@@ -53,6 +54,7 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: 'style.css',
         }),
+        new CleanWebpackPlugin(),
     ],
     mode: 'development',
     devServer: {
